2fbc985a7066fa7ec7c41b010cd53620ac5ec36f
karobar <travisp471@gmail.com>
2017-03-02 20:13:15 -0500

    Added default shaders

--- src/main/java/tjp/wiji/drawing/ShaderContext.java
+++ src/main/java/tjp/wiji/drawing/ShaderContext.java
@@ -11,14 +11,61 @@ import tjp.wiji.file.FileLoader;
 
 public class ShaderContext {
     private ShaderProgram shader;
+    private static final String DEFAULT_VERTEX_SHADER = 
+        "attribute vec4 a_position;\n" +
+        "attribute vec4 a_color;\n" +
+        "attribute vec2 a_texCoord0;\n" +
+        "attribute vec4 a_frontColor;\n" +
+        "attribute vec4 a_backColor;\n" +
+
+        "uniform mat4 u_projTrans;\n" +
+
+        "varying vec4 v_color;\n" +
+        "varying vec2 v_texCoords;\n" +
+        "varying vec4 v_frontColor;\n" +
+        "varying vec4 v_backColor;\n" +
+
+        "void main() {\n" +
+        "    v_color = a_color;\n" +
+        "    v_texCoords = a_texCoord0;\n" +
+        "    v_frontColor = a_frontColor;\n" +
+        "    v_backColor = a_backColor;\n" +
+
+        "    gl_Position = u_projTrans * a_position;\n" +
+        "}";
+    
+    private static final String DEFAULT_FRAGMENT_SHADER = 
+          "#ifdef GL_ES\n" +
+          "#define LOWP lowp\n" +
+          "    precision mediump float;\n" +
+          "#else\n" +
+          "    #define LOWP\n" +
+          "#endif\n" +
+
+          "varying LOWP vec4 v_color;\n" +
+          "varying vec2 v_texCoords;\n" +
+          "varying vec4 v_backColor;\n" +
+          "varying vec4 v_frontColor;\n" +
+
+          "uniform sampler2D u_texture;\n" +
+
+          "void main() {\n" +
+          "    vec4 switchColor = v_color * texture2D(u_texture, v_texCoords);\n" +
+          "    if(switchColor.r == 1.0f && switchColor.g == 1.0f && switchColor.b == 1.0f) {\n" +
+          "        gl_FragColor = v_frontColor;\n" +
+          "    }\n" +
+          "    else {\n" +
+          "        gl_FragColor = v_backColor;\n" +
+          "    }\n" +
+          "}";
     
     public ShaderContext() throws URISyntaxException {
         createShaders();
     }
     
     private void createShaders() throws URISyntaxException {
-        String vertexShader = makeFileHandle("vertex.glsl").readString();
-        String fragmentShader = makeFileHandle("fragment.glsl").readString();
+        String vertexShader = DEFAULT_VERTEX_SHADER;
+        String fragmentShader = DEFAULT_FRAGMENT_SHADER;
         shader = new ShaderProgram(vertexShader, fragmentShader);
         shader.pedantic = false;
     }
